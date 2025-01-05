44a0526fe4ca3ade74d7bab34d4f4cdaa623ef82
Mikestriken <michael.john.marais379@gmail.com>
2025-01-05 02:07:23 -0500

    added player movement and keybinds file

---
+++ CutThroat_UnityProject/Assets/FPS_Limiter.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class FPS_Limiter : MonoBehaviour
+{
+    private void Start() {
+        // Application.targetFrameRate = 30;
+    }
+}
--- CutThroat_UnityProject/Assets/Scripts/ActorScript.cs
+++ CutThroat_UnityProject/Assets/Scripts/ActorScript.cs
@@ -4,6 +4,27 @@ using UnityEngine.InputSystem;
 public class ActorScript : MonoBehaviour
 {
     public Rigidbody2D actorRigidBody;
+    public ActorKeybinds actorKeybinds;
+
+    private InputAction actorMoveKeys;
+    private InputAction actorAttackKeys;
+
+    private void Awake() {
+        actorKeybinds = new ActorKeybinds();
+    }
+    
+    private void OnEnable() {
+        actorMoveKeys = actorKeybinds.Player.Move;
+        actorMoveKeys.Enable();
+
+        actorAttackKeys = actorKeybinds.Player.Attack;
+        actorAttackKeys.Enable();
+    }
+
+    private void OnDisable() {
+        actorMoveKeys.Disable();
+        actorAttackKeys.Disable();
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,15 +35,29 @@ public class ActorScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        const float actorMoveSpeed = 1f;
+        updateMovement();
 
-        // switch (switch_on)
-        // {
-            
-        //     default:
-        // }
-        if (Input.GetKey(KeyCode.RightArrow))
-            actorRigidBody.linearVelocity = Vector2.right * actorMoveSpeed;
+    }
+
+    void updateMovement() {
+        const float actorMoveSpeed = 3f;
         
+        Vector2 moveDirection = Vector2.zero;
+        moveDirection = actorMoveKeys.ReadValue<Vector2>();
+
+        switch (moveDirection)
+        {
+            case Vector2 v when (v == Vector2.left || v == Vector2.right):
+                actorRigidBody.linearVelocity = moveDirection * actorMoveSpeed;
+                break;
+
+            case Vector2 v when v == Vector2.up:
+                actorRigidBody.linearVelocity = moveDirection * actorMoveSpeed;
+                break;
+
+            case Vector2 v when v == Vector2.down:
+
+                break;
+        }
     }
 }
